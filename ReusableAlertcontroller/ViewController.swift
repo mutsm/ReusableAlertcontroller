9e98ff5ccf8971c476c4bce98d98acec5d72837f
MW <mutasem@arafas.com>
2022-01-19 18:00:27 +0300

    Action added to cancel button

--- ReusableAlertcontroller/ViewController.swift
+++ ReusableAlertcontroller/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    class AlertView: NSObject {
+
+     class func showAlert(view: UIViewController , message: String){
+
+         let alert = UIAlertController(title: "Warning", message: message, preferredStyle: UIAlertController.Style.alert)
+         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+         alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { action  in
+             print("sdfsdfsdf")
+         }))
+         view.present(alert, animated: true, completion: nil)
+        }
     }
 
-
+    @IBAction func btnTapped(_ sender: UIButton) {
+        AlertView.showAlert(view: self, message: "Test alert")
+      
+        
+    }
 }
 
